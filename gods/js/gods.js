56849f31d9499970810e38a435b9538792917d70
Nicolas Ramz <nicolas.ramz@gmail.com>
2016-12-13 14:30:56 +0100

    IMPROVED: do not rely on jQuery anymore

--- gods/js/gods.js
+++ gods/js/gods.js
@@ -13,74 +13,72 @@ req.keys().forEach(function(key){
 });
 
 /*jshint devel: true*/
-$(function () {
-    class GodsClass extends Game {
-        constructor(options) {
-            super(options);
-            // intro
-            this.currentLevel = -1;
+class GodsClass extends Game {
+    constructor(options) {
+        super(options);
+        // intro
+        this.currentLevel = -1;
 
+    }
+    onEvent(event) {
+        switch (event.type) {
+            case 'game:gotoMenu':
+                this.setScene(sceneMenu);
+                break;
+
+            case 'game:startGame':
+                this.setScene(sceneLevel1);
+                break;
+
+            case 'game:gameover':
+            case 'game:exitLevel':
+                // this.stopScene();
+                this.setScene(sceneIntro);
+                break;
         }
-        onEvent(event) {
-            switch (event.type) {
-                case 'game:gotoMenu':
-                    this.setScene(sceneMenu);
-                    break;
+    }
+};
 
-                case 'game:startGame':
-                    this.setScene(sceneLevel1);
-                    break;
+window.Gods = new GodsClass({
+    debug: false,
+    name: 'Gods',
+    target: '.main',
+    showFps: true,
+    width: 1024,
+    height: 768,
+    sound: true
+});
 
-                case 'game:gameover':
-                case 'game:exitLevel':
-                    // this.stopScene();
-                    this.setScene(sceneIntro);
-                    break;
+window.Gods.onReady(function () {
+    // this.setScene(sceneIntro);
+    this.setScene(sceneLevel1);
+
+    // debug stuff
+    document.body.addEventListener('keyup', (event) => {
+        if (event.keyCode === 68) {
+            console.log('debug');
+            // scene debug
+            this.scene.debug();
+        }
+        if (event.keyCode === 82) {
+            this.scene.stop();
+
+            this.scene.resume();
+        }
+
+        if (event.keyCode === 80) {
+            this.togglePauseGame();
+        }
+
+        if (event.keyCode === 83) {
+            this.toggleSound(!that.sound);
+        }
+
+        if (event.keyCode === 72) {
+            if (this.scene.hudScene) {
+                var opacity = this.scene.hudScene.getOpacity();
+                this.scene.hudScene.setOpacity(opacity ? 0 : 1);
             }
         }
-    };
-
-    window.Gods = new GodsClass({
-        debug: false,
-        name: 'Gods',
-        target: '.main',
-        showFps: true,
-        width: 1024,
-        height: 768,
-        sound: true
-    });
-
-    window.Gods.onReady(function () {
-        // this.setScene(sceneIntro);
-        this.setScene(sceneLevel1);
-
-        // debug stuff
-        $('body').keyup( (event) => {
-            if (event.keyCode === 68) {
-                console.log('debug');
-                // scene debug
-                this.scene.debug();
-            }
-            if (event.keyCode === 82) {
-                this.scene.stop();
-
-                this.scene.resume();
-            }
-
-            if (event.keyCode === 80) {
-                this.togglePauseGame();
-            }
-
-            if (event.keyCode === 83) {
-                this.toggleSound(!that.sound);
-            }
-
-            if (event.keyCode === 72) {
-                if (this.scene.hudScene) {
-                    var opacity = this.scene.hudScene.getOpacity();
-                    this.scene.hudScene.setOpacity(opacity ? 0 : 1);
-                }
-            }
-        });
     });
 });
