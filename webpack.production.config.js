8b8161764c46ec5e7bc9df56c4992284bdf8320c
Nicolas Ramz <nicolas.ramz@gmail.com>
2017-07-01 22:44:10 +0200

    ADDED: production webpack configuration file

---
+++ webpack.production.config.js
@@ -0,0 +1,42 @@
+var path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+    entry: [
+        './gods/js/gods.js'
+    ],
+    output: {
+        path: __dirname,
+        filename: "bundle.js",
+        pathinfo: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader?presets[]=es2015',
+                exclude: /node_modules|athenajs\.js/
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            path.resolve('./gods/js'),
+            'node_modules'
+        ]
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Gods - AthenaJS',
+            template: 'index-wp.ejs',
+            inject: 'body'
+        }),
+        new WebpackNotifierPlugin({
+            alwaysNotify: true,
+            skipFirstNotification: true,
+            title: 'AthenaJS-Gods'
+        })
+    ]
+};
