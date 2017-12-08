b8414174b5a97b5cdde24466284982e4cdcca9ce
warpdesign <nicolas.ramz@gmail.com>
2017-12-08 08:50:05 +0100

    CLEANED-UP: console.log

--- gods/js/scenes/sceneIntro.js
+++ gods/js/scenes/sceneIntro.js
@@ -1,13 +1,10 @@
 import { Scene, Sprite, Menu, AudioManager as AM, InputManager as Input } from 'athenajs';
 
-console.log(Scene, Sprite, Menu, AM, Input);
-
 class SceneIntro extends Scene {
     constructor() {
         super({
             name: 'intro',
             resources: [
-                // images
                 {
                     id: 'intro',
                     type: 'image',
@@ -21,15 +18,10 @@ class SceneIntro extends Scene {
                 }
             ]
         });
-        console.log('retour super sceneIntro');
     }
 
     setup() {
         // this.setBackgroundImage('gods/img/godsIntro.jpg');
-        // super.onLoad();
-        // var that = this;
-
-        console.log('[scene ' + this.name + '] ' + 'setup');
 
         this.titleScreen = new Sprite('intro', {
             imageId: 'intro',
@@ -53,36 +45,12 @@ class SceneIntro extends Scene {
         this.addObject(this.titleScreen);
     }
     start() {
-        // super.start();
-
-        // var that = this;
-
-        // *** Input.clearEvents();
-
-        /*
-        Input.installKeyCallback('ENTER', 'up', () => {
-        });
-        */
-
         AM.play('restart');
 
         this.fadeInAndOut(1000, 2000, 1000).then(() => {
             this.notify('game:startGame');
         });
-        // this.fadeIn(1000, 2000).then(() => {
-        //     this.fadeOut(1000).then(() => {
-        //         console.log('fade ended');
-        //         this.notify('game:startGame');
-        //     });
-        // });
     }
-
-    // stop() {
-    //     Input.clearEvents();
-    //     super.stop();
-    // }
 };
-
-console.log('end sceneIntro');
 
 export default new SceneIntro();
